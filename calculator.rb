1d68fa0074685ac59fe51a22de428419f6f6291b
celebrated-integer-2023 <scailles23@gmail.com>
2019-08-05 05:05:28 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,9 +1,9 @@
-do
-first_number=1
-second_number=2
+
+first_number=>0
+second_number=>0
 sum=first_number+second_number
 difference=first_number-second_number
 product=first_number-second_number
-end
+
 
 
